bef64a33fce9f91b58eb752de67ee0b44d3445d9
AshleyMeagan <macnutta@live.com>
2024-11-26 12:26:40 -0400

    Messing around, moving things, deleting things

---
+++ Assets/Scripts/ZombieDoorTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+   public PlayableDirector timeline;
+   private void OnTriggerEnter(Collider other)
+    {
+      timeline.Play();  
+    }
+}
